f6c78a8db1746de7e6b726862e48765a799ef553
Felipe Bueno <felipe.bueno@osucateiro.com>
2021-02-08 09:06:59 -0300

    Scroll Infinito

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MainSectionContainer } from '../styles';
 import Feed from './Feed/Feed';
-import Loading from './Helper/Loading';
 
 const Home = () => {
   return (
